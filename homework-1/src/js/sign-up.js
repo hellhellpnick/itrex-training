f233a57b6ea937aeb9aa4cf3713689f34cb89557
hellpnick <hellhellpnick@gmail.com>
2021-10-27 13:23:55 +0300

    refactoring

--- homework-1/src/js/sign-up.js
+++ homework-1/src/js/sign-up.js
@@ -4,8 +4,6 @@ const inputsPassword = document.querySelectorAll('.form-main__wrapper--password'
 const inputEmail = document.querySelectorAll('.form-main__wrapper--email');
 const form = document.getElementsByTagName('form')[0];
 
-console.log(inputsPassword);
-
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 });
